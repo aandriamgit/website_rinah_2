a664279bd2423064093c1fccaf72c5e37e01b71e
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-09 05:34:17 +0300

    responsive fixed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import '../style/modern-normalize.css';
 import '../style/style.css';
 import '../style/components/header.css';
+import '../style/components/hero.css';
+import '../style/components/boxtwo.css';
 import '../style/utils.css';
