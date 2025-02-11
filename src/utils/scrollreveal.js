162f49f48cecfcb24e13ee52281eb3078e17334c
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-11 06:06:42 +0300

    small fix

--- src/utils/scrollreveal.js
+++ src/utils/scrollreveal.js
@@ -4,15 +4,15 @@ scrollreveal
 ({
 	// reset:true,
 	distance: '60px',
-	duration: 1000,
+	duration: 2500,
 });
 
-scrollreveal().reveal('.ooo', {delay: 100, origin: 'left', interval: 300});
+scrollreveal().reveal('.ooo', {delay: 300, origin: 'left', interval: 300});
 scrollreveal().reveal('.two', {delay: 200, origin: 'bottom', interval: 300});
 scrollreveal().reveal('.card', {delay: 300, origin: 'right', interval: 150});
-scrollreveal().reveal('.minicard', {delay: 1, origin: 'top', interval: 50});
+scrollreveal().reveal('.minicard', {delay: 180, origin: 'top', interval: 230});
 scrollreveal().reveal('.tb', {delay: 600, origin: 'bottom', interval: 200});
-scrollreveal().reveal('.ssbone', {delay: 280, origin: 'top', interval: 270});
+scrollreveal().reveal('.ssbone', {delay: 380, origin: 'top', interval: 270});
 scrollreveal().reveal('.miniboxlol', {delay: 630, origin: 'bottom', interval: 100});
 scrollreveal().reveal('.ssbtwo', {delay: 450, origin: 'left', interval: 190});
 scrollreveal().reveal('.faq-box__accordion', {delay: 450, origin: 'left', interval: 190});
