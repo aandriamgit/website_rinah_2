488a68d659998cd8a7fff089627e61e2010915b0
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-11 11:36:55 +0300

    ajout auto

--- src/utils/scrollreveal.js
+++ src/utils/scrollreveal.js
@@ -6,12 +6,12 @@ scrollreveal
 	duration: 1500,
 });
 
-scrollreveal().reveal('.ooo', {delay: 600, origin: 'left', interval: 300, easing: 'ease-in'});
-scrollreveal().reveal('.two', {delay: 500, origin: 'bottom', interval: 300});
-scrollreveal().reveal('.card', {delay: 200, origin: 'right', interval: 150});
-scrollreveal().reveal('.minicard', {delay: 480, origin: 'top', interval: 230});
-scrollreveal().reveal('.ssbone', {delay: 480, origin: 'top', interval: 270});
-scrollreveal().reveal('.faq-box__accordion', {delay: 650, origin: 'left', interval: 190});
+scrollreveal().reveal('.ooo', {delay: 200, origin: 'left', interval: 300, easing: 'ease-in'});
+scrollreveal().reveal('.two', {delay: 300, origin: 'bottom', interval: 300});
+scrollreveal().reveal('.card', {delay: 100, origin: 'right', interval: 150});
+scrollreveal().reveal('.minicard', {delay: 380, origin: 'top', interval: 230});
+scrollreveal().reveal('.ssbone', {delay: 280, origin: 'top', interval: 270});
+scrollreveal().reveal('.faq-box__accordion', {delay: 150, origin: 'left', interval: 190});
 
 
 export default scrollreveal;
