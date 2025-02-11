3392cac39340d3d1e0d27bba00c5b981984ef4b8
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-11 11:39:55 +0300

    ajout auto

--- src/utils/scrollreveal.js
+++ src/utils/scrollreveal.js
@@ -7,11 +7,10 @@ scrollreveal
 });
 
 scrollreveal().reveal('.ooo', {delay: 200, origin: 'left', interval: 300, easing: 'ease-in'});
-scrollreveal().reveal('.two', {delay: 300, origin: 'bottom', interval: 300});
-scrollreveal().reveal('.card', {delay: 100, origin: 'right', interval: 150});
-scrollreveal().reveal('.minicard', {delay: 380, origin: 'top', interval: 230});
-scrollreveal().reveal('.ssbone', {delay: 280, origin: 'top', interval: 270});
-scrollreveal().reveal('.faq-box__accordion', {delay: 150, origin: 'left', interval: 190});
+scrollreveal().reveal('.two', {delay: 300, origin: 'bottom', interval: 300, easing: 'ease-out'});
+scrollreveal().reveal('.minicard', {delay: 380, origin: 'top', interval: 230, easing: 'ease'});
+scrollreveal().reveal('.ssbone', {delay: 280, origin: 'top', interval: 270, easing: 'linear'});
+scrollreveal().reveal('.faq-box__accordion', {delay: 150, origin: 'left', interval: 190, easing: 'ease-in-out'});
 
 
 export default scrollreveal;
