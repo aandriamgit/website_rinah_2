951d0f51733558e67f0a8bea65e9d9191ee65abf
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-11 06:27:49 +0300

    ajout auto

--- src/utils/scrollreveal.js
+++ src/utils/scrollreveal.js
@@ -10,7 +10,6 @@ scrollreveal().reveal('.ooo', {delay: 600, origin: 'left', interval: 300});
 scrollreveal().reveal('.two', {delay: 500, origin: 'bottom', interval: 300});
 scrollreveal().reveal('.card', {delay: 700, origin: 'right', interval: 150});
 scrollreveal().reveal('.minicard', {delay: 480, origin: 'top', interval: 230});
-scrollreveal().reveal('.tb', {delay: 600, origin: 'bottom', interval: 200});
 scrollreveal().reveal('.ssbone', {delay: 480, origin: 'top', interval: 270});
 scrollreveal().reveal('.faq-box__accordion', {delay: 650, origin: 'left', interval: 190});
 
