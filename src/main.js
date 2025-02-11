c36ffe89875d2bd865d379a2cc570fffe28affcd
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-11 14:54:20 +0300

    analytics added

--- src/main.js
+++ src/main.js
@@ -16,7 +16,9 @@ import '../style/utils.css';
 import mobile_nav from './utils/mobile-nav';
 import faqs from './utils/faq';
 import ScrollReveal from './utils/my_scrollreveal.js';
+import { Analytics } from "@vercel/analytics/react";
 
 ScrollReveal();
 mobile_nav();
 faqs();
+Analytics();
