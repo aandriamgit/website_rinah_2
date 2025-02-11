a42fd3f78cd8bb77b6af6f4bed44ebcc001765c6
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-11 12:07:44 +0300

    ajout auto

--- src/utils/scrollreveal.js
+++ src/utils/scrollreveal.js
@@ -10,7 +10,7 @@ scrollreveal().reveal('.ooo', {delay: 200, origin: 'left', interval: 300, easing
 scrollreveal().reveal('.two', {delay: 300, origin: 'bottom', interval: 300, easing: 'ease-out'});
 scrollreveal().reveal('.minicard', {delay: 380, origin: 'top', interval: 230, easing: 'ease'});
 scrollreveal().reveal('.ssbone', {delay: 280, origin: 'top', interval: 270, easing: 'linear'});
-scrollreveal().reveal('.faq-box__accordion', {delay: 150, origin: 'left', interval: 190, easing: 'ease-in-out'});
+scrollreveal().reveal('.faq-box__accordion', {delay: 150, origin: 'left', interval: 190, easing: 'ease-out'});
 
 
 export default scrollreveal;
