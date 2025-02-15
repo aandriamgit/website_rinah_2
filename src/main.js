1c6b0d96115982318b578868ad34d72b1218fc08
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-15 13:00:44 +0300

    ajout auto

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import '../style/style.css';
 import mobile_nav from './utils/mobile-nav';
 import faqs from './utils/faq';
 import ScrollReveal from './utils/my_scrollreveal.js';
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics"
 
 mobile_nav();
 ScrollReveal();
 faqs();
-Analytics();
+inject();
