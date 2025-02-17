3b194ea92e3d73934ff0370f255a39af351966d9
aandriamgit <aandriam@student.42antananarivo.mg>
2025-02-17 19:06:00 +0300

    light mode added

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import '../style/modern-normalize.css';
 import '../style/style.css';
 
 import mobile_nav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
 import faqs from './utils/faq';
 import ScrollReveal from './utils/my_scrollreveal.js';
-import { inject } from "@vercel/analytics"
+import { inject } from "@vercel/analytics";
 
 mobile_nav();
+darkMode();
 ScrollReveal();
 faqs();
 inject();
